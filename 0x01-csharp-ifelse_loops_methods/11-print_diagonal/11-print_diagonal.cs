4ba68a8cc299b19a49968cf34081a59d172914db
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-07-14 14:01:49 +0100

    Printing diagonally

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length <= 0)
+            Console.WriteLine();
+
+        string c;
+        c = "\\";
+        for (int i = 0; i < length; i++)
+        {
+            Console.Write("{0}\n", c);
+            c = " " + c;
+        }
+    }
+}

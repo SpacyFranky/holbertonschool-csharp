281f6d5b56fefb24dbd8acf317388d9860c72d7e
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-07-14 10:58:11 +0100

    Printing all possible different combinations of two digits

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 8; i++)
+            {
+                if (i != 8)
+                {
+                    for (int j = i; j <= 9; j++)
+                    {
+                        if (i != j)
+                        Console.Write("{0}{1}, ", i, j);
+                    }
+                }
+                else
+                {
+                    int j = i + 1;
+                    Console.Write("{0}{1}\n", i, j);
+                }
+            }
+        }
+    }
+}

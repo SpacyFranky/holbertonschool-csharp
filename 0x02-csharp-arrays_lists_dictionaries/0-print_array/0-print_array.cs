a2c83789788320a7d1731a5985809bae86ea7d3f
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-07-16 12:20:42 +0100

    Printing an array of integers

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return(null);
+        }
+
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = i;
+            if (i != size - 1)
+                Console.Write("{0} ", array[i]);
+            else
+                Console.Write("{0}\n", array[i]);
+        }
+        return(array);
+    }
+}

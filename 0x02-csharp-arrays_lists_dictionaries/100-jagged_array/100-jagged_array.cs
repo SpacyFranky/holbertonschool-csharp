d31e100aa16627b328b6632591275443a57e82c8
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-07-16 22:29:09 +0100

    Jagged array

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int[][] jaggedArray = new int[3][];
+
+        jaggedArray[0] = new int[4] { 0, 1, 2, 3 };
+        jaggedArray[1] = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+        jaggedArray[2] = new int[2] { 0, 1 };
+
+        for (int i = 0; i < jaggedArray.Length; i++)
+        {
+            for (int j = 0; j < jaggedArray[i].Length; j++)
+                Console.Write("{0}{1}", jaggedArray[i][j], j == (jaggedArray[i].Length - 1) ? "" : " ");
+            Console.WriteLine();
+        }
+    }
+}

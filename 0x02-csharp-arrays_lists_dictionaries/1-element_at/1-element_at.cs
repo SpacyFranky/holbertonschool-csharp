616d15ca1a09249021f8057c42e797f45fe5556f
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-07-16 12:59:42 +0100

    removed unused int

--- 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -4,14 +4,14 @@ class Array
 {
     public static int elementAt(int[] array, int index)
     {
-        int i;
-
         if (index < 0 || index > array.Length)
         {
             Console.WriteLine("Index out of range");
             return(-1);
         }
         else
+        {
             return(array[index]);
+        }
     }
 }

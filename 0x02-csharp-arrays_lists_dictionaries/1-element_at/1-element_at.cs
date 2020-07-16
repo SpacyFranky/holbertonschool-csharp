fa746418a17ad65187bac9b51fee5e6f66d71918
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-07-16 12:50:24 +0100

    accessing an element with a for loop

--- 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -4,11 +4,15 @@ class Array
 {
     public static int elementAt(int[] array, int index)
     {
+        int i;
+
         if (index < 0 || index > array.Length)
         {
             Console.WriteLine("Index out of range");
             return(-1);
         }
-        return(array[index]);
+        for (i = 0; i < index; i++)
+            ;
+        return(array[i]);
     }
 }

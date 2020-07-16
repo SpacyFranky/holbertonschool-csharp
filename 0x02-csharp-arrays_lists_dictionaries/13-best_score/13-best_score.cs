ceed39e0c7b0ba388f638c0255f212d09d1f2575
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-07-16 16:45:29 +0100

    Best score in dictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList.Count == 0)
+            return("None");
+
+        int max = 0;
+        string key = "";
+        foreach(var item in myList)
+        {
+            if (myList[item.Key] > max)
+            {
+                max = myList[item.Key];
+                key = item.Key;
+            }
+        }
+        return(key);
+    }
+}

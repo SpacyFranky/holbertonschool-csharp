4e3b58423cf52c092dbef87bda04a788e1cb49f6
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-07-27 21:14:53 +0100

    Only differents

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> hashSet = new HashSet<int>();
+        foreach(int i in list1)
+        {
+            if(!list2.Contains(i))
+                hashSet.Add(i);
+        }
+        foreach(int i in list2)
+        {
+            if(!list1.Contains(i))
+                hashSet.Add(i);
+        }
+        List<int> newList = new List<int>(hashSet);
+        newList.Sort();
+        return(newList);
+    }
+}

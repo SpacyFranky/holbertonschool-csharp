9b8b68c50df770c336734e95378dad86562c8736
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-07-27 22:01:14 +0100

    Full stack

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aStack.Count());
+        if(aStack.Count() == 0)
+            Console.WriteLine("Stack is empty");
+        else
+        {
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+            if(aStack.Contains(search))
+            {
+                Console.WriteLine("Stack contains {0}: {1}", search, true);
+                int index = 0;
+                foreach(var i in aStack)
+                {
+                    if(i == search)
+                        break;
+                    index += 1;
+                }
+                while (aStack.Count >= index)
+                    aStack.Pop();
+                aStack.Push(newItem);
+            }
+            else
+                Console.WriteLine("Stack contains {0}: {1}", search, false);
+        }
+        return(aStack);
+    }
+}

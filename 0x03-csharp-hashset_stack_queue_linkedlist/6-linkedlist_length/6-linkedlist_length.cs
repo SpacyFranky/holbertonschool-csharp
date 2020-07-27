6630ddae574482541034a206f45238c5e0be6803
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-07-27 22:59:44 +0100

    LinkedList length

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int s = 0;
+        foreach(int i in myLList)
+            s += 1;
+        return(s);
+    }
+}

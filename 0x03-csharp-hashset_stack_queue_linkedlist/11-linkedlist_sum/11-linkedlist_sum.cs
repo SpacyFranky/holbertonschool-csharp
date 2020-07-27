e976a3d3baeb3f410a6d84e03d6b842c1aa3f688
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-07-27 23:41:58 +0100

    LinkedList sum

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int s = 0;
+        foreach (int i in myLList)
+            s += i;
+        return(s);
+    }
+}

230e691bf1ed247382e6784f47c489d697b7e6ca
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-07-27 23:38:16 +0100

    Get node at index

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int i = 0;
+        foreach (int number in myLList)
+        {
+            if (i == n)
+                return(number);
+            i++;
+        }
+        return(0);
+    }
+}

122a29ad76d8a38d6f1e0923e036bd333f1bb3bc
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-07-27 20:28:09 +0100

    Unique addition

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        var hashSet = new HashSet<int>(myList);
+        int s = 0;
+        foreach(int i in hashSet)
+        {
+            s += i;
+        }
+        return(s);
+    }
+}

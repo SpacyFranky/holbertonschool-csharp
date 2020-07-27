ce28c90f300095883ccded7c664811a123261949
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-07-27 20:38:34 +0100

    Present in both

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> newList = new List<int>();
+        newList = list1.FindAll(i => list2.Contains(i));
+        return(newList);
+    }
+}

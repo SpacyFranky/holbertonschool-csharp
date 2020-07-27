f3a74f6ebf72a7c01578c532e25a7d65e2f43f97
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-07-27 22:40:16 +0100

    Queue it up

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+        
+        if(aQueue.Count > 0)
+            Console.WriteLine("Top item: {0}", aQueue.Peek());
+        else
+            Console.WriteLine("Stack is empty");
+
+        if(aQueue.Contains(search))
+                Console.WriteLine("Stack contains \"{0}\": {1}", search, aQueue.Contains(search));
+            else
+                Console.WriteLine("Stack contains \"{0}\": {1}", search, aQueue.Contains(search));
+
+        if(aQueue.Contains(search))
+        {
+            while (aQueue.Contains(search))
+                    aQueue.Dequeue();
+        }
+
+        aQueue.Enqueue(newItem);
+
+        return(aQueue);
+    }
+}

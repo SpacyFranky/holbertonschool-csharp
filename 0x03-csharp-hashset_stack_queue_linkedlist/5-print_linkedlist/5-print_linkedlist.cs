8f894f1b774c02067192b2dcbebd79ff58ce9d9e
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-07-27 22:54:59 +0100

    Print a LinkedList

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> llist = new LinkedList<int>();
+
+        if (size < 0)
+            return(llist);
+
+        for(int i = 0; i < size; i++)
+        {
+            llist.AddLast(i);
+        }
+
+        foreach(int number in llist)
+            Console.WriteLine(number);
+
+        return(llist);
+    }
+}

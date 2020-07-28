9322dad8b9d0bf92901a91702bb947ff8d6b4f02
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-07-28 20:35:20 +0100

    Insert in order in linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> temp;
+
+        if (myLList.Count == 0)
+            return(null);
+
+        temp = myLList.First;
+        if (temp.Value >= n)
+            return(myLList.AddFirst(n));
+
+        while (temp.Next != null)
+        {
+            if (temp.Next.Value >= n)
+            {
+                temp = myLList.AddAfter(temp, n);
+                return(temp);
+            }
+            temp = temp.Next;
+        }
+
+        temp = myLList.AddLast(n);
+        return(temp);
+    }   
+}

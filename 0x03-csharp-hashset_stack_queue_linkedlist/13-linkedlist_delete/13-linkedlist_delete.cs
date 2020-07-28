4e24ed3a4e5fd9504303690f681052d1d8d3642f
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-07-28 20:56:42 +0100

    Delete at index

--- 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -6,6 +6,8 @@ class LList
     public static void Delete(LinkedList<int> myLList, int index)
     {
         LinkedListNode<int> temp = myLList.First;
+        if (index == 0)
+            myLList.Remove(myLList.First);
         while (index > 0)
         {
             temp = temp.Next;

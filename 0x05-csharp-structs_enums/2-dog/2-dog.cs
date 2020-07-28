7ec75918359248ecd554ab5ffe96f58747d336ed
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-07-28 16:52:32 +0100

    struct with parameters

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,23 @@
+﻿public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+public struct Dog
+{
+    public Dog(string n, float a, string o, Rating r)
+    {
+        name = n;
+        age = a;
+        owner = o;
+        rating = r;
+    }
+
+    public string name { get; }
+    public float age { get; }
+    public string owner { get; }
+    public Rating rating { get; }
+
+}

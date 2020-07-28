bde2a69c99d424dd89db7168ebbae3629806905e
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-07-28 11:01:14 +0100

    Integer division

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        try
+        {
+            Console.WriteLine("{0} / {1} = {2}", a, b, a / b);
+        }
+        catch
+        {
+            Console.WriteLine("Cannot divide by zero");
+            Console.WriteLine("{0} / {1} = {2}", a, b, 0);
+        }
+        finally
+        {
+        }
+    }
+}

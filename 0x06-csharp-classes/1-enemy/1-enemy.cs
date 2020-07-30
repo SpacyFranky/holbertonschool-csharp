4c6becd5c622256cdd6df9ba1dcadb7deff42074
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-07-30 20:15:21 +0100

    adding a public field and a public constructor to class Zombie

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/*
+    The main Enemies class
+*/ 
+namespace Enemies
+{
+    //  Creating an empty public class Zombie.
+    /// <summary>
+    /// Creating an empty public class Zombie.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Defining the class Zombie by a public filed.
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Defining the class Zombie by a public constructor.
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+
+}

7f9886231ec0257ef1876b96a01743d4cb78f5e8
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-08-04 12:17:39 +0100

    Adding XML documentation comments

--- 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -3,12 +3,12 @@
 namespace MyMath
 {
     /// <summary>
-    ///
+    /// Mathematical operations class
     /// </summary>
     public class Operations
     {
         /// <summary>
-        ///
+        /// Adding two integers
         /// </summary>
         public static int Add(int a, int b)
         {

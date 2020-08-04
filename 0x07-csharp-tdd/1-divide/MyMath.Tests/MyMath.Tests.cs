c19afb9e4e938bcc3b89d21f2167be601b9871aa
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-08-04 14:49:07 +0100

    Dividing a matrix

---
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+
+namespace MyMath.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            // Arrange
+            int[,] matrix = new int[,]
+            {
+                {2, 4, 6},
+                {8, 10, 12}
+            };
+            int [,] new_matrix = { {1, 2, 3}, {4, 5, 6} };
+            int num = 2;
+
+            // Act
+            int [,] divmatrix = Matrix.Divide(matrix, num);
+
+            // Assert
+            Assert.AreEqual(new_matrix, divmatrix);
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyMath
+{
+    public class Matrix
+    {
+        public static int[,] Divide(int[,] matrix, int num)
+        {
+            if (matrix == null)
+                return(null);
+
+            int[,] divmatrix = new int[matrix.GetLength(0), matrix.GetLength(1)];
+            for(int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for(int j = 0; j < matrix.GetLength(1); j++)
+                    try
+                    {
+                        divmatrix[i, j] = matrix[i, j] / num;
+                    }
+                    catch
+                    {
+                        throw new System.ArgumentException("Num cannot be 0");
+                        return(null);
+                    }
+            }
+            return(divmatrix);
+        }
+    }
+}

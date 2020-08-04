5622891e73e72af45181dba59751d8cf8c2aac6b
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-08-04 23:50:33 +0100

    camelCase

---
+++ 0x07-csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+
+namespace Text.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(0, Str.CamelCase(""));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(1, Str.CamelCase("hello"));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual(2, Str.CamelCase("hello There"));
+        }
+
+        [Test]
+        public void Test4()
+        {
+            Assert.AreEqual(1, Str.CamelCase("hello there"));
+        }
+
+        [Test]
+        public void Test5()
+        {
+            Assert.AreEqual(2, Str.CamelCase("hello there Friend"));
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/5-camelcase/Text/Text.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Text
+{
+    /// <summary>
+    /// Str class
+    /// </summary>
+    public class Str
+    {
+        /// <summary>
+        /// Determines how many words are in a string. Each word begins with a capital
+        /// letter except the first word.
+        /// </summary>
+        public static int CamelCase(string s)
+        {
+            int words = 0;
+            if (s.Length == 0)
+                return(0);
+            
+            if (char.IsLower(s[0]))
+            {
+                for (int i = 0 ; i < s.Length; i++)
+                {
+                    if (s[i] == ' ')
+                    {
+                        if ((i+1 < s.Length) && (char.IsUpper(s[i+1])))
+                            words += 1;
+                    }
+                }
+                return(words+1);
+            }
+            return(words);
+        }
+    }
+}

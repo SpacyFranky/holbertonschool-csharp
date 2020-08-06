21f4e23ea8bd701c61d26458c68ba0cb358a4e67
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-08-06 10:46:37 +0100

    Only subclass

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/// <summary>
+/// Object Class
+/// </summary>
+public class Obj
+{
+    /// <summary>
+    /// Returns True if the object is an instance of a class that inherits from the specified class, 
+    /// otherwise return False. The object must be a subclass.
+    /// </summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType == baseType)
+            return true;
+        else
+            return false;
+    }
+}

628d66dd38a39e0f092862c89efbad0471750aa6
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-08-06 12:15:38 +0100

    Basic shapes

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// Shape Class
+/// </summary>
+public class Shape
+{
+    /// <summary>
+    /// Throws an NotImplementedException.
+    /// </summary>
+    public virtual int Area()
+    {
+        throw new System.NotImplementedException("Area() is not implemented");
+    }
+}

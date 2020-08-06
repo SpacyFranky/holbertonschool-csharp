9b5498a6421a21b5d736097c80438b01a3048d40
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-08-06 07:56:31 +0100

    Checking type of object

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/// <summary>
+/// Object Class
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Returns True if the object is an int, otherwise Flase.
+    /// </summary>
+    public static bool IsOfTypeInt(object obj)
+    {
+        if (obj is int)
+            return true;
+        else
+            return false;
+    }
+}
+

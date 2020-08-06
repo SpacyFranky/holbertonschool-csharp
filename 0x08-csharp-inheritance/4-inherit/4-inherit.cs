625423eb1866642d83aeae5469bdf0a44ea15c3d
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-08-06 12:04:12 +0100

    Inheriting a class

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary>
+/// Animal Class
+/// </summary>
+public class Animal {}
+
+/// <summary>
+/// Dog Class that inherits from Animal class.
+/// </summary>
+public class Dog : Animal {}

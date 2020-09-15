e32d80f7155fcdfc518cc0703527b8c980645bde
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-09-15 12:25:38 +0100

    Vector-scalar multiplication

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// VectorMath Class
+/// </summary>
+class VectorMath
+{
+    /// <summary>
+    /// Multiplies a vector and a scalar and returns the resulting vector.
+    /// </summary>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        if (vector.Length == 2 )
+        {
+            double[] new_vector = new double[2];
+            for(int i = 0; i < vector.Length; i++)
+                new_vector[i] = vector[i] * scalar;
+            return(new_vector);
+        }
+        else if (vector.Length == 3)
+        {
+            double[] new_vector = new double[3];
+            for(int i = 0; i < vector.Length; i++)
+                new_vector[i] = vector[i] * scalar;
+            return(new_vector);
+        }
+        else
+        {
+            double[] new_vector = new double[] {-1};
+            return(new_vector);
+        }
+    }
+}

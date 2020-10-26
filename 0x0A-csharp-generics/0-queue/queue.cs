f0c834f821b74d470f857af9616a9f998674dfca
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-10-26 21:34:41 +0100

    Queue

---
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// New Queue Class
+/// </summary>
+public class Queue<T>
+{
+    /// <summary>
+    /// Public method that returns the Queue's type
+    /// </summary>
+    public string CheckType()
+    {
+        string QueueType = typeof(T).ToString();
+        return(QueueType);
+    }
+}

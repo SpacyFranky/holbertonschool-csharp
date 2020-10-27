8cf5b54853f8225365253f7698bafb77811052c7
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-10-28 00:28:22 +0100

    Abstract class

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/// <summary>
+///
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public string name;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public override string ToString()
+    {
+        Type t = this.GetType();
+        return name + " is a " + t;
+    }
+}

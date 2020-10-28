51258563844f37cebf1d23b828846a8b4fd72f25
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-10-28 10:54:49 +0100

    Setting up an escape room

---
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+/// <summary>
+/// Abstract Base class
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Public attribute name
+    /// </summary>
+    public string name;
+
+    /// <summary>
+    /// Public method that prints name and type
+    /// </summary>
+    public override string ToString()
+    {
+        Type t = this.GetType();
+        return name + " is a " + t;
+    }
+}
+
+/// <summary>
+/// Interface
+/// </summary>
+interface IInteractive
+{
+    void Interact();
+}
+
+/// <summary>
+/// Interface
+/// </summary>
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+/// <summary>
+/// Interface
+/// </summary>
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+/// <summary>
+/// Public Door class
+/// </summary>
+public class Door : Base, IInteractive
+{
+    /// <summary>
+    /// Door constructor
+    /// </summary>
+    public Door(string value)
+    {
+        if (name == null || name == "")
+            name = "Door";
+        
+        name = value;
+    }
+
+    /// <summary>
+    /// Public method Interact
+    /// </summary>
+    public void Interact()
+    {
+        Console.WriteLine("You try to open the {0}. It's locked.", name);
+    }
+
+
+}

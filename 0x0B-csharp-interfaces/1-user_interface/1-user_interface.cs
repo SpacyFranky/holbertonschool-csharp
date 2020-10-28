a245b36d849df7eda08a87edb225ec792f91af9e
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-10-28 10:19:04 +0100

    Adding interfaces

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+/// <summary>
+/// Abstract Base class
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Public attribute name
+    /// </summary>
+    public string name;
+
+    /// <summary>
+    /// Public method that prints name and type
+    /// </summary>
+    public override string ToString()
+    {
+        Type t = this.GetType();
+        return name + " is a " + t;
+    }
+}
+
+/// <summary>
+/// interface
+/// </summary>
+interface IInteractive
+{
+    void Interact();
+}
+
+/// <summary>
+/// interface
+/// </summary>
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+/// <summary>
+/// Interface
+/// </summary>
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+/// <summary>
+/// Public TestObject class
+/// </summary>
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    /// <summary>
+    /// Public method Interact
+    /// </summary>
+    public void Interact()
+    {
+    
+    }
+
+    /// <summary>
+    /// Public object
+    /// </summary>
+    public int durability { get; set; }
+    /// <summary>
+    /// Public method Break
+    /// </summary>
+    public void Break()
+    {
+
+    }
+
+    /// <summary>
+    /// Public object
+    /// </summary>
+    public bool isCollected { get; set; }
+    /// <summary>
+    /// Public method Collect
+    /// </summary>
+    public void Collect()
+    {
+
+    }
+}

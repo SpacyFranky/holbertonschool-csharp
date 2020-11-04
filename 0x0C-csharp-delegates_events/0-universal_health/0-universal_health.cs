8a5b8020439459f635f78253368ab59926047e72
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-11-04 14:10:59 +0100

    Fixed edge case for maxHp and hp value

--- 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -17,14 +17,17 @@ public class Player
     public Player(string name = "Player", float maxHp = 100f)
     {
         this.name = name;
-        if (maxHp <= 0)
+        if (maxHp > 0.0f)
         {
-            this.maxHp = 100f;
-            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = maxHp;
+            this.hp = maxHp;
         }
         else
-            this.maxHp = maxHp;
-        this.hp = maxHp;
+        {
+            this.maxHp = 100f;
+            this.hp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
     }
 
     /// <summary>

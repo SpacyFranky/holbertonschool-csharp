58ecd81fab423af48899f102cd567a47b5ada9f4
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-11-04 13:59:29 +0100

    Setting player class and printing health method

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/// <summary>
+/// Player class
+/// </summary>
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    /// <summary>
+    /// Class Constructor
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="maxHp"></param>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+        if (maxHp <= 0)
+        {
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        else
+            this.maxHp = maxHp;
+        this.hp = maxHp;
+    }
+
+    /// <summary>
+    /// Public method that prints health
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine("{0} has {1} / {2} health", this.name, this.hp, this.maxHp);
+    }
+}

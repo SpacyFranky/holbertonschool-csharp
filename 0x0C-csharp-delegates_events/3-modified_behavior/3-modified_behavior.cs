d69c7b850dadd66387d89288f29fc2a112a801ce
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-12-18 23:00:11 +0100

    Using delegates

---
+++ 0x0C-csharp-delegates_events/3-modified_behavior/3-modified_behavior.cs
@@ -0,0 +1,139 @@
+﻿using System;
+
+/// <summary>
+/// Player class
+/// </summary>
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    /// <summary>
+    /// Public float amount of health
+    /// </summary>
+    public delegate float CalculateHealth();
+
+    /// <summary>
+    /// Public method that prints the taken damage
+    /// </summary>
+    /// <param name="damage"></param>
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0)
+        {
+            Console.WriteLine("{0} takes 0 damage!", this.name);
+        }
+        else
+        {
+            ValidateHP(this.hp - damage);
+            Console.WriteLine("{0} takes {1} damage!", this.name, damage);
+        }
+    }
+
+    /// <summary>
+    /// Public method that prints the healed damage
+    /// </summary>
+    /// <param name="heal"></param>
+    public void HealDamage(float heal)
+    {
+        if (heal < 0)
+        {
+            Console.WriteLine("{0} heals 0 HP!", this.name);
+        }
+        else
+        {
+            ValidateHP(this.hp + heal);
+            Console.WriteLine("{0} heals {1} HP!", this.name, heal);
+        }
+    }
+
+    /// <summary>
+    /// Public method that sets the new value of the player's hp
+    /// </summary>
+    /// <param name="newHp"></param>
+    public void ValidateHP(float newHp)
+    {
+        if (newHp < 0f)
+            this.hp = 0f;
+        else if (newHp > this.maxHp)
+            this.hp = this.maxHp;
+        else
+            this.hp = newHp;
+    }
+
+    /// <summary>
+    /// Class Constructor
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="maxHp"></param>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+        if (maxHp > 0f)
+        {
+            this.maxHp = maxHp;
+            this.hp = maxHp;
+        }
+        else
+        {
+            this.maxHp = 100f;
+            this.hp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+    }
+
+    /// <summary>
+    /// Public method that prints health
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine("{0} has {1} / {2} health", this.name, this.hp, this.maxHp);
+    }
+
+
+    /// <summary>
+    /// Applies modifier
+    /// </summary>
+    /// <param name="baseValue"></param>
+    /// <param name="modifier"></param>
+    /// <returns></returns>
+    public float ApplyModifier(float baseValue, Modifier modifier)
+    {
+        if (modifier is Modifier.Weak)
+            return(baseValue / 2f);
+        else if (modifier is Modifier.Base)
+            return(baseValue);
+        else
+            return(baseValue * 1.5f);
+    }
+}
+
+
+/// <summary>
+/// Modifier
+/// </summary>
+public enum Modifier
+{
+    /// <summary>
+    /// Weak
+    /// </summary>
+    Weak,
+    /// <summary>
+    /// Base
+    /// </summary>
+    Base,
+    /// <summary>
+    /// Strong
+    /// </summary>
+    Strong
+}
+
+
+/// <summary>
+/// Modifier
+/// </summary>
+/// <param name="baseValue"></param>
+/// <param name="modifier"></param>
+/// <returns></returns>
+public delegate float CalculateModifier(float baseValue, Modifier modifier);
